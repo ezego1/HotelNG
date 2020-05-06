656871cf8da4c34e60cd685e007fa5ae509ec9c6
Nwabueze Nwaiwu <ezegodwin24@yahoo.com>
2020-05-06 21:59:08 +0100

    route modification

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ mongoose
     .then(() => console.log("mongodb connected"))
     .catch((err) => console.log(err));
 
-app.use("/api/users", users);
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/subject", require("./routes/api/subject"));
 
 const port = process.env.PORT || 3000;
 
